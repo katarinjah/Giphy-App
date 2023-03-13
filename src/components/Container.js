daf4338335a0eaafe0708523085a7cb25a0dfb78
katarinjah <katarina.d.262@gmail.com>
2023-03-13 22:18:52 +0100

    Task completed

--- src/components/Container.js
+++ src/components/Container.js
@@ -5,21 +5,23 @@ export const Container = () => {
     const [searchInput, setSearchInput] = useState("");
     const [gifs, setGifs] = useState([]);
 
+    const fetchGifs = (query) => {
+        const apiKey = "bLS2gJF6VVc2vKqvA49G176qixlOEWLf"
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}`;
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            setGifs(data.data)
+        })
+        .catch(error => console.log(error));
+    };
+
     const onSearchHandler = (event) => {
         if (event.key === "Enter") {
             fetchGifs(searchInput);
             setSearchInput("");
         };
     };
-
-    const fetchGifs = (query) => {
-        const apiKey = "bLS2gJF6VVc2vKqvA49G176qixlOEWLf"
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}`;
-        fetch(url)
-        .then(response => response.json())
-        .then(data => setGifs(data.data))
-        .catch(error => console.log(error));
-        };
     
     useEffect(() => {
         if (searchInput) {
@@ -38,10 +40,10 @@ export const Container = () => {
                 onKeyDown={onSearchHandler}
                 />
             </div>
-            <div id="gifBox">
+            <div className="grid-container">
                 {gifs.map((gif) => (
-                    <div className="gif">
-                        <img key={gif.id} src={gif.images.original.url} alt={gif.title} />
+                    <div className="grid-item">
+                        <img key={gif.id} src={gif.images.downsized.url} alt={gif.title} />
                     </div>
                 ))}
             </div>
